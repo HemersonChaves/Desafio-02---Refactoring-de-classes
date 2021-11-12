40e6f4a65530e3aea69bbb3c8d63f0ab4dde1f85
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-12 09:50:36 -0300

    feat(hook): add hook for management of order

---
+++ src/hooks/pedidos.tsx
@@ -0,0 +1,24 @@
+import { IFood } from '../types';
+
+
+const addFood = async (foodtId: number) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+const updateFood = async (foodtId: number) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+const deleteFood = async (food: IFood) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
