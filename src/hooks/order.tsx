bf3bcca004e6ed1b4511a4e01f4c146fc1e0469a
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-12 10:31:03 -0300

    refactor(hook): preparing for implementation of orders addFood, updateFood and deleteFood

---
+++ src/hooks/order.tsx
@@ -0,0 +1,46 @@
+import { createContext, ReactNode, useState } from 'react';
+import { IFood } from '../types';
+
+interface IOrderProviderProps {
+    children: ReactNode;
+}
+interface OrderContextData {
+    order: IFood[];
+    addFood: (productId: number) => Promise<void>;
+    updateFood: (productId: number) => void;
+    deleteFood: (productId: number, food: IFood) => void;
+}
+const OrderContext = createContext<OrderContextData>({} as OrderContextData);
+
+export function OrderProvider({ children }: IOrderProviderProps) {
+    const [order, setOrder] = useState<IFood[]>([]);
+
+    const addFood = async (foodtId: number) => {
+        try {
+            //todo
+        } catch (error) {
+            //todo
+        }
+    }
+    const updateFood = async (foodtId: number) => {
+        try {
+            //todo
+        } catch (error) {
+            //todo
+        }
+    }
+    const deleteFood = async (productId: number, food: IFood) => {
+        try {
+            //todo
+        } catch (error) {
+            //todo
+        }
+    }
+    return (
+        <OrderContext.Provider
+            value={{ order, addFood , updateFood, deleteFood }}
+        >
+            {children}
+        </OrderContext.Provider>
+    )
+}
