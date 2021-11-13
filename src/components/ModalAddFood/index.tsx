b15765567bc488fc90a3bec1c891a3058a6f3581
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-13 14:06:13 -0300

    refactor(ModalAddForm): set inputs for a HandleAddFood

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,41 +1,72 @@
-import { Component, createRef } from 'react';
+import { FormEvent, SetStateAction, useState } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
 import { OrderFood } from '../../hooks/order';
+import { IFood } from '../../types';
 
-interface FormElements extends HTMLFormControlsCollection {
-  yourInputName: HTMLInputElement
+interface ITargetSetStateAction { 
+  target: { 
+    value: SetStateAction<string>; 
+  }; 
 }
 
-interface YourFormElement extends HTMLFormElement {
-  readonly elements: FormElements
-}
 
 interface IModalAddFoodProps {
   setIsOpen: () => void;
   isOpen: boolean;
+  handleAddFood:  (food: IFood) => Promise<void> 
 }
 
-const ModalAddFood = ({setIsOpen, isOpen}: IModalAddFoodProps) => {
-  const { order, addFood } = OrderFood();
+const ModalAddFood = ({ setIsOpen, isOpen, handleAddFood }: IModalAddFoodProps) => {
+  //const { order, addFood } = OrderFood();
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [price, setPrice] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
 
-  const handleFormSubmit = (e: React.FormEvent<YourFormElement>) => {
-    e.preventDefault();
-    console.log(e.currentTarget.elements.yourInputName.value)
+  async function handleFormSubmit(event: FormEvent) {
+    //event.preventDefault();
+    const food: IFood = {
+      name,
+      image: imageUrl,
+      price,
+      description,
+      available: true
+    }
+    await handleAddFood(food);
   }
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
       <Form onSubmit={handleFormSubmit}>
         <h1>Novo Prato</h1>
-        <Input name="image" placeholder="Cole o link aqui" icon={undefined} />
+        <Input
+          name="image"
+          placeholder="Cole o link aqui"
+          icon={undefined}
+          onChange={(event: ITargetSetStateAction) => setImageUrl(event.target.value)} />
 
-        <Input name="name" placeholder="Ex: Moda Italiana" icon={undefined} />
-        <Input name="price" placeholder="Ex: 19.90" icon={undefined} />
+        <Input
+          name="name"
+          placeholder="Ex: Moda Italiana"
+          icon={undefined}
+          onChange={(event:ITargetSetStateAction) => setName(event.target.value)}
+        />
+        <Input 
+        name="price" 
+        placeholder="Ex: 19.90" 
+        icon={undefined} 
+        onChange={(event:ITargetSetStateAction) => setPrice(event.target.value)}
+        />
 
-        <Input name="description" placeholder="Descrição" icon={undefined} />
+        <Input 
+        name="description" 
+        placeholder="Descrição" 
+        icon={undefined} 
+        onChange={(event:ITargetSetStateAction) => setDescription(event.target.value)}
+        />
         <button type="submit" data-testid="add-food-button">
           <p className="text">Adicionar Prato</p>
           <div className="icon">
